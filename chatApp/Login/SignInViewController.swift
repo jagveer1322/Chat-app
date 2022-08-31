0a6bfb885561f6e990da2a5d96cf4ccd6d9ff087
jagveer singh <jagveersingh.55555@gmail.com>
2022-08-31 09:57:45 +0530

    intial

---
+++ chatApp/Login/SignInViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SignInViewController.swift
+//  chatApp
+//
+//  Created by Macbook on 29/08/22.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func signInButton(_ sender: UIButton) {
+    }
+}
---
+++ chatApp/Login/SignUpViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SignUpViewController.swift
+//  chatApp
+//
+//  Created by Macbook on 29/08/22.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
